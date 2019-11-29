64e63c724ccfd74e4a0cef11a0f6494d62f2ccbd
Duong Nguyen <Duong.Nguyen@miracles.vn>
2019-11-29 20:32:20 +0700

    up

--- resources/views/frontend/edit.blade.php
+++ resources/views/frontend/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.frontend.index')
 
-@section('title', 'Register')
+@section('title', 'Account')
 
 @section('css')
 <style type="text/css">
@@ -17,7 +17,7 @@
 		<div class="container">
 			<div class="breadcrumb-banner d-flex flex-wrap align-items-center justify-content-end">
 				<div class="col-first">
-					<h1>Register</h1>
+					<h1>Account</h1>
 					<nav class="d-flex align-items-center">
 						<a href="/">Home<span class="lnr lnr-arrow-right"></span></a>
 						<a href="">Register</a>
